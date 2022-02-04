58ea49a923ad37c5d925c0c4e2e35c611c21cd9f
badzi <badzi007@gmail.com>
2022-02-04 16:01:17 +0100

    adapte scripte to new DataBase code

--- SCRIPT_CREATION/script_creation_capeb.sql
+++ SCRIPT_CREATION/script_creation_capeb.sql
@@ -1,155 +1,133 @@
-create table ENTREPRISE (
-   _NOENTREPRISE__      SERIAL                 ,
-   NOSIRET              TEXT                 null,
-   NOM_ENTREPRISE                TEXT                 null,
-   EFFECTIF             INT4                 null,
-   ANNEEDECREATION      INT4                 null,
-   constraint PK_ENTREPRISE primary key (_NOENTREPRISE__)
+-- public.entreprise definition
+-- Drop table
+-- DROP TABLE public.entreprise;
+CREATE TABLE public.entreprise (
+	noentreprise serial4 NOT NULL,
+	anneedecreation int4 NULL,
+	effectif int4 NULL,
+	nosiret varchar(255) NULL,
+	nom_entreprise varchar(255) NULL,
+	CONSTRAINT entreprise_pkey PRIMARY KEY (noentreprise)
 );
-
-create table COMPTE (
-   ID_COMPTE            SERIAL               ,
-   _NOENTREPRISE__      INT4                 not null,
-   IS_ADMIN             BOOL                 not null,
-   MAIL                 TEXT                 not null,
-   PWD                  TEXT                 not null,
-   _NAME                TEXT                 null,
-   FIRST_NAME           TEXT                 null,
-   IS_VALID             BOOL                 not null,
-   CREATION_DATE        DATE                 null,
-   LAST_LOGIN           DATE                 null,
-   IS_CGU               BOOL                 null,
-   IS_FORGOTPASS        BOOL                 null,
-   IS_TEMP_PASSWORD     TEXT                 null,
-   constraint PK_COMPTE primary key (ID_COMPTE)
+-- public.metier definition
+-- Drop table
+-- DROP TABLE public.metier;
+CREATE TABLE public.metier (
+	id_metier serial4 NOT NULL,
+	nom_metier varchar(255) NOT NULL,
+	CONSTRAINT metier_pkey PRIMARY KEY (id_metier)
 );
-
-create table CATEGORIE_QUESTION (
-   ID_CATEGORIE         SERIAL               ,
-   ID_QUESTIONNAIRE     INT4                 not null,
-   libelle              TEXT                 not null,
-   constraint PK_CATEGORIE_QUESTION primary key (ID_CATEGORIE)
+-- public.questionnaire definition
+-- Drop table
+-- DROP TABLE public.questionnaire;
+CREATE TABLE public.questionnaire (
+	id_questionnaire serial4 NOT NULL,
+	thematique varchar(255) NOT NULL,
+	CONSTRAINT questionnaire_pkey PRIMARY KEY (id_questionnaire),
+	CONSTRAINT ukhnqfog8s68hf67f8ysrrnj0st UNIQUE (thematique)
 );
-
-create table EVALUATION (
-   ID_EVALUATION        SERIAL               ,
-   ID_COMPTE            INT4                 not null,
-   constraint PK_EVALUATION primary key (ID_EVALUATION)
+-- public.categorie_question definition
+-- Drop table
+-- DROP TABLE public.categorie_question;
+CREATE TABLE public.categorie_question (
+	id_categorie serial4 NOT NULL,
+	libelle varchar(255) NOT NULL,
+	id_questionnaire int4 NOT NULL,
+	CONSTRAINT categorie_question_pkey PRIMARY KEY (id_categorie),
+	CONSTRAINT fkdrxl0k0ggi4rajmluvo4nnkoc FOREIGN KEY (id_questionnaire) REFERENCES public.questionnaire(id_questionnaire)
 );
-
-create table METIER (
-   ID_METIER            SERIAL               ,
-   NOM_METIER           TEXT                not null,
-   constraint PK_METIER primary key (ID_METIER)
+-- public.compte definition
+-- Drop table
+-- DROP TABLE public.compte;
+CREATE TABLE public.compte (
+	id_compte serial4 NOT NULL,
+	creation_date date NULL,
+	first_name varchar(255) NULL,
+	is_admin bool NULL,
+	is_cgu bool NULL,
+	is_forgotpass bool NULL,
+	is_temp_password varchar(255) NULL,
+	is_valid bool NULL,
+	last_login date NULL,
+	mail varchar(255) NULL,
+	"name" varchar(255) NULL,
+	pwd varchar(255) NULL,
+	noentreprise int4 NOT NULL,
+	CONSTRAINT compte_pkey PRIMARY KEY (id_compte),
+	CONSTRAINT fk53wyy5y93qsjp6v32an1grte3 FOREIGN KEY (noentreprise) REFERENCES public.entreprise(noentreprise)
 );
-
-create table METIER_QUESTION (
-   ID_QUESTION          INT4                 not null,
-   ID_METIER            INT4                 not null,
-   constraint PK_METIER_QUESTION primary key (ID_QUESTION, ID_METIER)
+-- public.evaluation definition
+-- Drop table
+-- DROP TABLE public.evaluation;
+CREATE TABLE public.evaluation (
+	id_evaluation serial4 NOT NULL,
+	id_compte int4 NOT NULL,
+	CONSTRAINT evaluation_pkey PRIMARY KEY (id_evaluation),
+	CONSTRAINT fkfw17o0msl05lapxwndpi2qof7 FOREIGN KEY (id_compte) REFERENCES public.compte(id_compte)
 );
-
-create table PRECONISATION_CATEGORIE (
-   ID_PRECONISATION     SERIAL               ,
-   ID_CATEGORIE         INT4                 not null,
-   contenu     text        null,
-   viewIfPourcentageScoreLessThan       INT4                 null,
-   constraint PK_PRECONISATION_CATEGORIE primary key (ID_PRECONISATION)
+-- public.preconisation_categorie definition
+-- Drop table
+-- DROP TABLE public.preconisation_categorie;
+CREATE TABLE public.preconisation_categorie (
+	id_preconisation serial4 NOT NULL,
+	contenu text NULL,
+	view_if_pourcentage_score_less_than int4 NULL,
+	id_categorie int4 NOT NULL,
+	CONSTRAINT preconisation_categorie_pkey PRIMARY KEY (id_preconisation),
+	CONSTRAINT fk1tinogp2sxqftkin8bfw0jp8e FOREIGN KEY (id_categorie) REFERENCES public.categorie_question(id_categorie)
 );
-
-create table PRECONISATION_GLOBALE (
-   ID_PRECONISATION_G   SERIAL               ,
-   ID_QUESTIONNAIRE     INT4                 not null,
-   CONTENU              TEXT                 null,
-   viewIfPourcentageScoreLessThan       INT4                 null,
-   constraint PK_PRECONISATION_GLOBALE primary key (ID_PRECONISATION_G)
+-- public.preconisation_globale definition
+-- Drop table
+-- DROP TABLE public.preconisation_globale;
+CREATE TABLE public.preconisation_globale (
+	id_preconisation_g serial4 NOT NULL,
+	contenu text NULL,
+	view_if_pourcentage_score_less_than int4 NULL,
+	id_questionnaire int4 NOT NULL,
+	CONSTRAINT preconisation_globale_pkey PRIMARY KEY (id_preconisation_g),
+	CONSTRAINT fkedtr0ur7drk38mudmpjtwijl6 FOREIGN KEY (id_questionnaire) REFERENCES public.questionnaire(id_questionnaire)
 );
-
-CREATE TYPE TYPE_QUESTION AS ENUM ('Question Choix Unique', 'Question Choix Multiple');
-
-create table QUESTION (
-   ID_QUESTION          SERIAL                 ,
-   ID_CATEGORIE         INT4                 not null,
-   TYPE                 TYPE_QUESTION        null,
-   LIBELLE_QUESTION     TEXT                 null,
-   constraint PK_QUESTION primary key (ID_QUESTION)
+-- public.question definition
+-- Drop table
+-- DROP TABLE public.question;
+CREATE TABLE public.question (
+	id_question serial4 NOT NULL,
+	aide varchar(255) NULL,
+	libelle_question text NULL,
+	q_type varchar(255) NULL,
+	id_categorie int4 NOT NULL,
+	CONSTRAINT question_pkey PRIMARY KEY (id_question),
+	CONSTRAINT fkj58hfx2dxu7wje6poknogkwfn FOREIGN KEY (id_categorie) REFERENCES public.categorie_question(id_categorie)
 );
-
-create table QUESTIONNAIRE (
-   ID_QUESTIONNAIRE     SERIAL               ,
-   THEMATIQUE           TEXT                not null,
-   constraint PK_QUESTIONNAIRE primary key (ID_QUESTIONNAIRE)
+-- public.reponse definition
+-- Drop table
+-- DROP TABLE public.reponse;
+CREATE TABLE public.reponse (
+	id_reponse serial4 NOT NULL,
+	contenu text NULL,
+	nb_points int4 NULL,
+	id_question int4 NOT NULL,
+	CONSTRAINT reponse_pkey PRIMARY KEY (id_reponse),
+	CONSTRAINT fk7utui46seipahjep29ytvvwep FOREIGN KEY (id_question) REFERENCES public.question(id_question)
 );
-
-create table REPONSE (
-   ID_REPONSE           SERIAL               ,
-   ID_QUESTION          INT4                 not null,
-   NB_POINTS            INT4                 not null,
-   CONTENU              TEXT                 null,
-   constraint PK_REPONSE primary key (ID_REPONSE)
+-- public.score_category definition
+-- Drop table
+-- DROP TABLE public.score_category;
+CREATE TABLE public.score_category (
+	id_categorie int4 NOT NULL,
+	id_evaluation int4 NOT NULL,
+	nb_points int4 NULL,
+	CONSTRAINT score_category_pkey PRIMARY KEY (id_categorie, id_evaluation),
+	CONSTRAINT fk78v7np6darpj0o1dp1n8f3viw FOREIGN KEY (id_evaluation) REFERENCES public.evaluation(id_evaluation),
+	CONSTRAINT fk8wjhr1aeaoeq1gmogf3ryanhb FOREIGN KEY (id_categorie) REFERENCES public.categorie_question(id_categorie)
 );
-
-
-create table SCORE_CATEGORY (
-   ID_EVALUATION        INT4                 not null,
-   ID_CATEGORIE         INT4                 not null,
-   NB_POINTS            INT4                 null,
-   constraint PK_SCORE_CATEGORY primary key (ID_EVALUATION, ID_CATEGORIE)
+-- public.metier_question definition
+-- Drop table
+-- DROP TABLE public.metier_question;
+CREATE TABLE public.metier_question (
+	id_metier int4 NOT NULL,
+	id_question int4 NOT NULL,
+	CONSTRAINT metier_question_pkey PRIMARY KEY (id_metier, id_question),
+	CONSTRAINT fki0m5r78avsaiuo327pwqhl40k FOREIGN KEY (id_question) REFERENCES public.question(id_question),
+	CONSTRAINT fktlbo1il3jml4knpfe6kqx7jm7 FOREIGN KEY (id_metier) REFERENCES public.metier(id_metier)
 );
-
-
-
-alter table SCORE_CATEGORY
-   add constraint FK_ASSOCIAT_ASSOCIATI_EVALUATI foreign key (ID_EVALUATION)
-      references EVALUATION (ID_EVALUATION)
-      on delete restrict on update restrict;
-
-alter table SCORE_CATEGORY
-   add constraint FK_ASSOCIAT_ASSOCIATI_CATEGORI foreign key (ID_CATEGORIE)
-      references CATEGORIE_QUESTION (ID_CATEGORIE)
-      on delete restrict on update restrict;
-
-alter table CATEGORIE_QUESTION
-   add constraint FK_CATEGORI_ASSOCIATI_QUESTION foreign key (ID_QUESTIONNAIRE)
-      references QUESTIONNAIRE (ID_QUESTIONNAIRE)
-      on delete restrict on update restrict;
-
-alter table COMPTE
-   add constraint FK_COMPTE_ASSOCIATI_ENTREPRI foreign key (_NOENTREPRISE__)
-      references ENTREPRISE (_NOENTREPRISE__)
-      on delete restrict on update restrict;
-
-alter table EVALUATION
-   add constraint FK_EVALUATI_ASSOCIATI_COMPTE foreign key (ID_COMPTE)
-      references COMPTE (ID_COMPTE)
-      on delete restrict on update restrict;
-
-alter table METIER_QUESTION
-   add constraint FK_METIER_Q_METIER_QU_QUESTION foreign key (ID_QUESTION)
-      references QUESTION (ID_QUESTION)
-      on delete restrict on update restrict;
-
-alter table METIER_QUESTION
-   add constraint FK_METIER_Q_METIER_QU_METIER foreign key (ID_METIER)
-      references METIER (ID_METIER)
-      on delete restrict on update restrict;
-
-alter table PRECONISATION_CATEGORIE
-   add constraint FK_PRECONIS_ASSOCIATI_CATEGORI foreign key (ID_CATEGORIE)
-      references CATEGORIE_QUESTION (ID_CATEGORIE)
-      on delete restrict on update restrict;
-
-alter table PRECONISATION_GLOBALE
-   add constraint FK_PRECONIS_ASSOCIATI_QUESTION foreign key (ID_QUESTIONNAIRE)
-      references QUESTIONNAIRE (ID_QUESTIONNAIRE)
-      on delete restrict on update restrict;
-
-alter table QUESTION
-   add constraint FK_QUESTION_ASSOCIATI_CATEGORI foreign key (ID_CATEGORIE)
-      references CATEGORIE_QUESTION (ID_CATEGORIE)
-      on delete restrict on update restrict;
-
-alter table REPONSE
-   add constraint FK_REPONSE_ASSOCIATI_QUESTION foreign key (ID_QUESTION)
-      references QUESTION (ID_QUESTION)
-      on delete restrict on update restrict;
--- Script_Insertion/script_insertion.sql
+++ Script_Insertion/script_insertion.sql
@@ -1,9 +1,7 @@
 INSERT INTO ENTREPRISE (NOSIRET,NOM_ENTREPRISE,EFFECTIF,ANNEEDECREATION ) VALUES ('1234','abc',5,2000);
 INSERT INTO ENTREPRISE (NOSIRET,NOM_ENTREPRISE,EFFECTIF,ANNEEDECREATION ) VALUES ('AG6778','CZPLomberie',5,2000);
-
-
-insert  into compte (_noentreprise__,is_admin,mail,pwd,_name,first_name,is_valid,creation_date,last_login,is_cgu,is_forgotpass,is_temp_password) values (1,true,'a@gmail.com','pwd','rob','rob',true,'1999-01-08','1999-01-08',true,false,false);
-insert  into compte (_noentreprise__,is_admin,mail,pwd,_name,first_name,is_valid,creation_date,last_login,is_cgu,is_forgotpass,is_temp_password) values (2,true,'b@gmail.com','pwd2','bob','bob',true,'2000-01-08','2000-01-08',true,false,false);
+insert  into compte (noentreprise,is_admin,mail,pwd,name,first_name,is_valid,creation_date,last_login,is_cgu,is_forgotpass,is_temp_password) values (1,true,'a@gmail.com','pwd','rob','rob',true,'1999-01-08','1999-01-08',true,false,false);
+insert  into compte (noentreprise,is_admin,mail,pwd,name,first_name,is_valid,creation_date,last_login,is_cgu,is_forgotpass,is_temp_password) values (2,true,'b@gmail.com','pwd2','bob','bob',true,'2000-01-08','2000-01-08',true,false,false);
 
 
 insert  into questionnaire  (thematique) values ('Securité pour les meties du batiments');
@@ -24,16 +22,16 @@ insert  into metier  (nom_metier) values ('Menuisier');
 insert  into metier  (nom_metier) values ('Charpentier');
 insert  into metier  (nom_metier) values ('Maçon');
 
-insert  into question  (id_categorie,"type",libelle_question) values (4,'Question Choix Unique','Vos salariés ont-ils à disposition des masques anti-poussière');
-insert  into question  (id_categorie,"type",libelle_question) values (2,'Question Choix Unique','Déplacez-vous les matériaux au plus près de la zone de chantier');
-insert  into question  (id_categorie,"type",libelle_question) values (1,'Question Choix Multiple','Quels sont les EPI à disposition des salariés');
-insert  into question  (id_categorie,"type",libelle_question) values (1,'Question Choix Multiple','Proposez-vous d''éventuelles formations à vos salariés parmi les choix suivants? ');
+insert  into question  (id_categorie,q_type,libelle_question) values (4,'Question Choix Unique','Vos salariés ont-ils à disposition des masques anti-poussière');
+insert  into question  (id_categorie,q_type,libelle_question) values (2,'Question Choix Unique','Déplacez-vous les matériaux au plus près de la zone de chantier');
+insert  into question  (id_categorie,q_type,libelle_question) values (1,'Question Choix Multiple','Quels sont les EPI à disposition des salariés');
+insert  into question  (id_categorie,q_type,libelle_question) values (1,'Question Choix Multiple','Proposez-vous d''éventuelles formations à vos salariés parmi les choix suivants? ');
 
-insert  into preconisation_globale  (id_questionnaire,contenu,viewIfPourcentageScoreLessThan) values (1,'En France, le risque électrique représente plus de 30% des incendies et plus de 10 morts par an',70);
-insert  into preconisation_globale  (id_questionnaire,contenu,viewIfPourcentageScoreLessThan) values (1,'Selon l’art. R4544-9 et 10, l’habilitation électrique pour non électricien est obligatoire, en cas d’accident du travail la responsabilité civile ou pénale de l’employeur pourra être engagée. L’employeur s’expose aux versements de dommages et intérêts et à une amende',80);
+insert  into preconisation_globale  (id_questionnaire,contenu,view_if_pourcentage_score_less_than) values (1,'En France, le risque électrique représente plus de 30% des incendies et plus de 10 morts par an',70);
+insert  into preconisation_globale  (id_questionnaire,contenu,view_if_pourcentage_score_less_than) values (1,'Selon l’art. R4544-9 et 10, l’habilitation électrique pour non électricien est obligatoire, en cas d’accident du travail la responsabilité civile ou pénale de l’employeur pourra être engagée. L’employeur s’expose aux versements de dommages et intérêts et à une amende',80);
 
-insert  into preconisation_categorie  (id_categorie ,contenu,viewIfPourcentageScoreLessThan) values (2,'L’organisation du chantier est très importante, elle vous permettra d’éviter le port de charges inutiles et donc de réduire les risques physiques liées à la manutention manuelle qui représente 48% des accidents du travail',70);
-insert  into preconisation_categorie  (id_categorie ,contenu,viewIfPourcentageScoreLessThan) values (4,'En 2019, l’amiante est à l’origine d’environ 3 000 maladies professionnelles et de 1 800 cancers. Si les moyens de protection nécessaires concernant ce risque ne sont pas respectés, vous risquez 1 an d’emprisonnement et 75 000 € d’amende pour mise en danger de la vie d’autrui',80);
+insert  into preconisation_categorie  (id_categorie ,contenu,view_if_pourcentage_score_less_than) values (2,'L’organisation du chantier est très importante, elle vous permettra d’éviter le port de charges inutiles et donc de réduire les risques physiques liées à la manutention manuelle qui représente 48% des accidents du travail',70);
+insert  into preconisation_categorie  (id_categorie ,contenu,view_if_pourcentage_score_less_than) values (4,'En 2019, l’amiante est à l’origine d’environ 3 000 maladies professionnelles et de 1 800 cancers. Si les moyens de protection nécessaires concernant ce risque ne sont pas respectés, vous risquez 1 an d’emprisonnement et 75 000 € d’amende pour mise en danger de la vie d’autrui',80);
 
 insert  into metier_question  (id_question, id_metier) values (1,6);
 insert  into metier_question  (id_question, id_metier) values (1,7);
@@ -56,4 +54,3 @@ insert into reponse (id_question, nb_points, contenu) values (2,6,'Oui');
 insert into reponse (id_question, nb_points, contenu) values (2,0,'Non');
 insert into reponse (id_question, nb_points, contenu) values (2,3,'Certains');
 insert into reponse (id_question, nb_points, contenu) values (2,6,'Non Concerné');
-
